9277f7f7b386c607c29640c0b0a21bc54a873f65
Manu <76539155+Manu4559@users.noreply.github.com>
2023-11-16 12:58:08 +0100

    gh-pages add

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  https:'//Manu4559.github.io/vite-react-example'
+  https:'/vite-react-example/'
 })
